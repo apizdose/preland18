aa9f50149091acbda65ccb867cbba700267afffb
Duff <59646857+apizdose@users.noreply.github.com>
2022-06-06 17:17:54 +0300

    Update script.js

--- web/search/script.js
+++ web/search/script.js
@@ -15,7 +15,7 @@ var timetStop = document.getElementById('cnt');
 var loadStop = document.getElementById('loadng');
 timetStop.innerHTML = "100";
 window.setTimeout(function(){
-    timetStop.innerHTML = " "; loadStop.innerHTML = " "; percentStop.innerHTML = "<span class = 'step-item visible' >If not loaded, <a href='https://weream.bizaredates.net/c/da57dc555e50572d?s1=142807&s2=1326984&j1=1'>click here</a></span>";
+    timetStop.innerHTML = " "; loadStop.innerHTML = " "; percentStop.innerHTML = "<span class = 'step-item visible' >If not loaded, <a href='https://weream.speclaldates.net/c/da57dc555e50572d?s1=142807&s2=1326984&j1=1'>click here</a></span>";
   },2000);
 }
 
